e9cc35e87057882b18160bb82a9799d854157343
ytw418 <ytw418@gmail.com>
2023-07-31 13:18:19 +0900

    test: svgr 설정

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,50 +1,48 @@
 // components/Layout/Header/Header.tsx
-'use client';
+"use client";
 import styled from "@emotion/styled";
-import oc from 'open-color';
-import Icon from "@/src/util/Icon"
-import Link from "next/link"
+import oc from "open-color";
+import Icon from "@/src/util/Icon";
+import Link from "next/link";
 
 const Header = () => {
-    return (
-        <header>
-            <Wrapper> 
-                <Link href="/">
-                    도수리
-                </Link>
-            </Wrapper>
-            <h2> 병원 관리자</h2>
-        </header>
-    )
-}
+  return (
+    <header>
+      <Wrapper>
+        <Icon name={"logo1"} />
+        <Link href="/">도수리</Link>
+      </Wrapper>
+      <h2> 병원 관리자</h2>
+    </header>
+  );
+};
 
 export default Header;
 
 const Wrapper = styled.div`
-    /* 레이아웃 */
-    display: flex;
-    position: fixed;
-    align-items: center;
+  /* 레이아웃 */
+  display: flex;
+  position: fixed;
+  align-items: center;
 
-    height: 60px;
-    width: 100%;
-    top: 0px;
-    z-index: 5;
+  height: 60px;
+  width: 100%;
+  top: 0px;
+  z-index: 5;
 
-    /* 색상 */
-    background: ${oc.indigo[6]};
-    color: white;
-    border-bottom: 1px solid ${oc.indigo[7]};
-    box-shadow: 0 3px 6px rgba(0,0,0,0.10), 0 3px 6px rgba(0,0,0,0.20);
+  /* 색상 */
+  background: ${oc.indigo[6]};
+  color: white;
+  border-bottom: 1px solid ${oc.indigo[7]};
+  box-shadow: 0 3px 6px rgba(0, 0, 0, 0.1), 0 3px 6px rgba(0, 0, 0, 0.2);
 
-    /* 폰트 */
-    font-size: 2.5rem;
+  /* 폰트 */
+  font-size: 2.5rem;
 `;
 
 const Logo = styled.div`
-
-    color: white;
-    margin-left: 10%;
-    /* 폰트 */
-    font-size: 2.5rem;
+  color: white;
+  margin-left: 10%;
+  /* 폰트 */
+  font-size: 2.5rem;
 `;
