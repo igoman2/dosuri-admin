54ed23b8a108a753dc7310dde814563b142e1f27
SCHeo <tjheo@ajou.ac.kr>
2023-08-03 10:50:21 +0900

    added SideBar

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -1,9 +1,12 @@
 import Header from "@/src/components/Layout/Header/Header";
+import SideBar from "@/src/components/Layout/SideBar/SideBar";
+
 // 메인 경로의 레이아웃입니다.
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <div>
       <Header />
+      <SideBar />
       {children}
     </div>
   );
